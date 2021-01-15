1b0bdb72eedb3c50cda5011e90e22e29e9800807
José Antonio Duarte Júnior <duarte936@gmail.com>
2021-01-15 15:01:23 -0300

    Create Exercicio_12.cob

---
+++ Exercicio_12/Exercicio_12.cob
@@ -0,0 +1,178 @@
+      ******************************************************************
+      * Author:JOSE ANTONIO DUARTE JR.80x25
+      * Date:14/01/2021
+      * Purpose:COBOL PARA MICROCOMPUTADORES EXERCICIO:8 PAG:127
+      * Tectonics: cobc
+      * Objetivo: LISTAGEM DE PESQUISA COM TABELA
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. EXER12.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES. DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT TABELA ASSIGN TO DISK
+                  ORGANIZATION IS SEQUENTIAL
+                  ACCESS MODE IS SEQUENTIAL
+                  FILE STATUS IS WS-STATUS.
+           SELECT ARQMAR ASSIGN TO DISK
+                  ORGANIZATION IS SEQUENTIAL
+                  ACCESS MODE IS SEQUENTIAL
+                  FILE STATUS IS WS-STATUS.
+           SELECT RELATO ASSIGN TO PRINTER.
+       DATA DIVISION.
+       FILE SECTION.
+       FD  TABELA  LABEL RECORD STANDARD
+           VALUE OF FILE-ID "TABELA".
+       01  REG-TABELA.
+           02  FD-CODIGO     PIC 9(03)    VALUE ZEROS.
+           02  FD-MODELO     PIC X(15)    VALUE SPACES.
+           02  FD-VALOR      PIC 9(7)V99  VALUE ZEROS.
+       FD  ARQMAR  LABEL RECORD STANDARD
+           VALUE OF FILE-ID "ARQMAR".
+       01  REG-ARQMAR.
+           02  FD-MATRICULA  PIC 9(06) VALUE ZEROS.
+           02  FD-NOME       PIC X(30) VALUE SPACES.
+           02  FD-CIDADE     PIC X(20) VALUE SPACES.
+           02  FD-ESTADO     PIC XX.
+           02  FD-CODIGO2    PIC 99.
+       FD  RELATO LABEL RECORD OMITTED LINAGE 58 TOP 2 BOTTOM 5.
+       01  REG-RELATO PIC X(249).
+       WORKING-STORAGE SECTION.
+       77  CONFIRMA          PIC X(01)     VALUE SPACES.
+       77  WS-STATUS         PIC 9(02)     VALUE ZEROS.
+       77  SUB               PIC 9(03)     VALUE 1.
+       77  CT-PAG            PIC 9(02)     VALUE ZEROS.
+       77  TOTAL-REGS-LIDOS  PIC 9(03)     VALUE ZEROS.
+       77  TOTAL-REGS-IMPR   PIC 9(03)     VALUE ZEROS.
+       77  TOTAL-REGS-DESPRE PIC 9(03)     VALUE ZEROS.
+       01  DATA-SISTEMA.
+           02 ANO            PIC 9(02) VALUE ZEROS.
+           02 MES            PIC 9(02) VALUE ZEROS.
+           02 DIA            PIC 9(02) VALUE ZEROS.
+       01  TABELA-1 OCCURS 20 TIMES.
+           02 ELEM-TABELA-CODIGO      PIC 9(03).
+           02 ELEM-TABELA-MODELO      PIC X(15).
+           02 ELEM-TABELA-VALOR       PIC 9(7)V99.
+       01  CAB01.
+           02 FILLER         PIC X(02)    VALUE SPACES.
+           02 FILLER         PIC X(06)    VALUE "DATA:".
+           02 DATA-CAB01.
+               03 DIA-CAB01  PIC 99/ VALUE ZEROS.
+               03 MES-CAB01  PIC 99/ VALUE ZEROS.
+               03 ANO-CAB01  PIC 99  VALUE ZEROS.
+           02 FILLER         PIC X(04)     VALUE SPACES.
+           02 FILLER         PIC X(37)
+           VALUE "RELACAO DE CLIENTES CONTEMPLADOS PAG:".
+           02 PAG-CAB01      PIC Z9     VALUE ZEROS.
+       01  CAB02.
+           02 FILLER         PIC X(09) VALUE "MATRICULA".
+           02 FILLER         PIC X(03) VALUE SPACES.
+           02 FILLER         PIC X(30) VALUE "NOME".
+           02 FILLER         PIC X(06) VALUE SPACES.
+           02 FILLER         PIC X(20) VALUE "CIDADE".
+           02 FILLER         PIC X(04) VALUE SPACES.
+           02 FILLER         PIC X(06) VALUE "ESTADO".
+           02 FILLER         PIC X(03) VALUE SPACES.
+           02 FILLER         PIC X(15) VALUE "MODELO".
+           02 FILLER         PIC X(04) VALUE SPACES.
+           02 FILLER         PIC X(12) VALUE "PRECO".
+       01  DETALHE.
+           02 DET-MATRICULA  PIC 9(06) VALUE ZEROS.
+           02 FILLER         PIC X(06) VALUE SPACES.
+           02 DET-NOME       PIC X(30) VALUE SPACES.
+           02 FILLER         PIC X(06) VALUE SPACES.
+           02 DET-CIDADE     PIC X(20) VALUE SPACES.
+           02 FILLER         PIC X(04) VALUE SPACES.
+           02 DET-ESTADO     PIC XX.
+           02 FILLER         PIC X(07) VALUE SPACES.
+           02 DET-MODELO     PIC X(15) VALUE SPACES.
+           02 DET-VALOR      PIC Z.ZZZ.ZZ9,99.     
+       01  RODAPE.
+           02 FILLER         PIC X(07) VALUE SPACES.
+           02 DET-TEXTO      PIC X(35).
+           02 FILLER         PIC X(02) VALUE SPACES.
+           02 DET-TOTAIS     PIC ZZ9.
+       PROCEDURE DIVISION.
+       INICIO.
+           OPEN INPUT TABELA.
+           IF WS-STATUS NOT = "00"
+              IF WS-STATUS NOT = "30"
+                  DISPLAY "ARQUIVO TABELA NAO ENCONTRADO " AT 2020
+                  STOP RUN
+               ELSE
+               DISPLAY "ARQUIVO DANIFICADO" AT 2020
+               DISPLAY "CODIGO STATUS = " WS-STATUS AT 2120
+               STOP RUN
+           ELSE
+              NEXT SENTENCE
+           END-IF.
+           OPEN INPUT ARQMAR.
+           IF WS-STATUS NOT = "00"
+              IF WS-STATUS NOT = "30"
+                  DISPLAY "ARQUIVO ARQMAR NAO ENCONTRADO " AT 2020
+                  STOP RUN
+               ELSE
+               DISPLAY "ARQUIVO DANIFICADO" AT 2020
+               DISPLAY "CODIGO STATUS = " WS-STATUS AT 2120
+               STOP RUN
+           ELSE
+              NEXT SENTENCE
+           END-IF.
+           OPEN OUTPUT RELATO.
+           ACCEPT DATA-SISTEMA FROM DATE.
+           MOVE DIA TO DIA-CAB01.
+           MOVE MES TO MES-CAB01.
+           MOVE ANO TO ANO-CAB01.
+       CABECALHO.
+           MOVE SPACES TO REG-RELATO.
+           ADD 1 TO CT-PAG.
+           MOVE CT-PAG TO PAG-CAB01.
+           WRITE REG-RELATO FROM CAB01 BEFORE ADVANCING 2 LINES.
+           WRITE REG-RELATO FROM CAB02 BEFORE ADVANCING 2 LINES.
+       LER-CARREGAR-TABELA.
+           READ TABELA AT END GO TO FECHAMENTO.
+           MOVE REG-TABELA TO TABELA-1(SUB).
+           ADD 1 TO SUB.
+           IF SUB < 20
+             GO TO LER-CARREGAR-TABELA
+           ELSE
+               GO TO NEXT SENTENCE
+           END-IF.
+       LER-CARREGAR-ARQMAR.
+           READ ARQMAR AT END GO TO FECHAMENTO.
+           MOVE 0 TO SUB.
+           ADD 1 TO TOTAL-REGS-LIDOS.
+           PERFORM BUSCA-REGISTRO UNTIL SUB =19.
+           GO TO LER-CARREGAR-ARQMAR.
+       BUSCA-REGISTRO.
+           ADD 1 TO SUB.
+           IF ELEM-TABELA-CODIGO(SUB) = FD-CODIGO2
+                MOVE FD-MATRICULA TO DET-MATRICULA
+                MOVE FD-NOME TO DET-NOME
+                MOVE FD-CIDADE TO DET-CIDADE
+                MOVE FD-ESTADO TO DET-ESTADO
+                MOVE ELEM-TABELA-MODELO(SUB) TO DET-MODELO
+                COMPUTE DET-VALOR = 
+           (ELEM-TABELA-VALOR(SUB) + (ELEM-TABELA-VALOR(SUB) * 0,75))
+                WRITE REG-RELATO FROM DETALHE BEFORE ADVANCING 2 LINES
+                ADD 1 TO TOTAL-REGS-IMPR
+           END-IF.
+       FECHAMENTO.
+           MOVE TOTAL-REGS-LIDOS TO DET-TOTAIS.     
+           MOVE "TOTAL DE CLIENTES CADASTRADOS.....>" TO DET-TEXTO.
+           WRITE REG-RELATO FROM RODAPE AFTER ADVANCING 3 LINES.
+           MOVE TOTAL-REGS-IMPR TO DET-TOTAIS.
+           MOVE "TOTAL DE REGISTROS IMPRESSOS......>" TO DET-TEXTO.
+           WRITE REG-RELATO FROM RODAPE BEFORE ADVANCING 1 LINES.
+           COMPUTE TOTAL-REGS-DESPRE=TOTAL-REGS-LIDOS - TOTAL-REGS-IMPR.
+           MOVE TOTAL-REGS-DESPRE TO DET-TOTAIS.
+           MOVE "TOTAL DE REGISTROS DESPREZADOS....>" TO DET-TEXTO.
+           WRITE REG-RELATO FROM RODAPE BEFORE ADVANCING 1 LINES.
+           CLOSE TABELA ARQMAR RELATO.
+       FIM.
+           DISPLAY "PROGRAMA EXECUTADO COM SUCESSO" AT 0101.
+           ACCEPT CONFIRMA AT 0201.
+           STOP RUN.
+           END PROGRAM EXER12.
